c9bf98199558063a057c951efe9d186105c62ccb
Tiya Hans <tiyahans@Tishas-MacBook-Air.local>
2026-06-09 09:16:27 +0530

    Added working mp4 output

--- final_autonomous_system.py
+++ final_autonomous_system.py
@@ -36,9 +36,9 @@ fps = int(cap.get(cv2.CAP_PROP_FPS))
 # OUTPUT VIDEO
 ####################################
 
-output_path = "outputs/final_output.avi"
+output_path = "outputs/final_output.mp4"
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
 out = cv2.VideoWriter(
     output_path,
